de7ea07bd85632669eda31265c7698bba0936c64
Ganijon Rahimov <ganijon.rahimov@se.com>
2020-02-24 11:25:08 -0500

    add graph-ql api test stubs

---
+++ src/api/Playground.Api.GraphQL.Test/SubscriptionTestBase.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Playground.Api.GraphQL.Test
+{
+    public class SubscriptionTestBase
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
