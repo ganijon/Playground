07d3ffdf2aa3d5699c2e3763e5a79b761ab88780
Ganijon Rahimov <ganijon.rahimov@se.com>
2020-02-27 19:22:34 -0500

    add .vscode launch settings for api

--- src/api/Playground.Api/Startup.cs
+++ src/api/Playground.Api/Startup.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using GraphQL;
 using GraphQL.Server;
 using GraphQL.Server.Ui.GraphiQL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -21,123 +16,111 @@ using Playground.Graph.Schema.Types;
 using Playground.Graph.Services;
 using Playground.Repository;
 
-namespace Playground.Api
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace Playground.Api {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices (IServiceCollection services) {
             // Enable any Cross-Origin Requests
-            services.AddCors();
+            services.AddCors ();
 
             // Repository of mock data
-            services.AddSingleton<IRepository, MockRepository>();
+            services.AddSingleton<IRepository, MockRepository> ();
 
             // Services
-            services.AddSingleton<IAuthorService, AuthorService>();
-            services.AddSingleton<IBookService, BookService>();
+            services.AddSingleton<IAuthorService, AuthorService> ();
+            services.AddSingleton<IBookService, BookService> ();
 
             // REST endpoints
-            services.AddControllers()
-                .AddNewtonsoftJson(options => 
-                { 
-                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore; 
+            services.AddControllers ()
+                .AddNewtonsoftJson (options => {
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
                 });
 
             // GraphQL endpoints
-            ConfigureGraphQL(services);
+            ConfigureGraphQL (services);
         }
 
-        private void ConfigureGraphQL(IServiceCollection services)
-        {
+        private void ConfigureGraphQL (IServiceCollection services) {
             #region Kestrel
             // If using Kestrel:
 #if NETCOREAPP3_1
             // Workaround until GraphQL can swap off Newtonsoft.Json and onto the new MS one.
             // Depending on whether you're using IIS or Kestrel, the code required is different
             // See: https://github.com/graphql-dotnet/graphql-dotnet/issues/1116
-            services.Configure<KestrelServerOptions>(options =>
-            {
+            services.Configure<KestrelServerOptions> (options => {
                 options.AllowSynchronousIO = true;
             });
-            services.Configure<IISServerOptions>(options =>
-            {
+            services.Configure<IISServerOptions> (options => {
                 options.AllowSynchronousIO = true;
             });
 #endif
             #endregion
 
             // Register DepedencyResolver; this will be used when a GraphQL type needs to resolve a dependency
-            services.AddSingleton<IDependencyResolver>(x =>
-                new FuncDependencyResolver(type => x.GetRequiredService(type)));
+            services.AddSingleton<IDependencyResolver> (x =>
+                new FuncDependencyResolver (type => x.GetRequiredService (type)));
 
             // Schema
-            services.AddSingleton<TheSchema>();
+            services.AddSingleton<TheSchema> ();
 
             // Query
-            services.AddSingleton<TheQuery>();
+            services.AddSingleton<TheQuery> ();
 
             // Mutation
-            services.AddSingleton<TheMutation>();
-            services.AddSingleton<AuthorRequest>();
-            services.AddSingleton<BookRequest>();
+            services.AddSingleton<TheMutation> ();
+            services.AddSingleton<AuthorRequest> ();
+            services.AddSingleton<BookRequest> ();
 
             // Subscription
-            services.AddSingleton<TheSubscription>();
+            services.AddSingleton<TheSubscription> ();
 
             // Types
-            services.AddSingleton<AuthorType>();
-            services.AddSingleton<PublisherType>();
-            services.AddSingleton<BookType>();
+            services.AddSingleton<AuthorType> ();
+            services.AddSingleton<PublisherType> ();
+            services.AddSingleton<BookType> ();
 
             // Register GraphQL services
-            services.AddGraphQL(options =>
-            {
+            services.AddGraphQL (options => {
                 options.EnableMetrics = true;
                 options.ExposeExceptions = true;
-            }).AddWebSockets();
+            }).AddWebSockets ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
-            app.UseCors(builder => builder
-                .AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader());
+            app.UseCors (builder => builder
+                .AllowAnyOrigin ()
+                .AllowAnyMethod ()
+                .AllowAnyHeader ());
 
-            app.UseHttpsRedirection();
+            app.UseHttpsRedirection ();
 
             // This will enable WebSockets in Asp.Net core
-            app.UseWebSockets();
+            app.UseWebSockets ();
 
             // Enable endpoint for websockets (subscriptions)
-            app.UseGraphQLWebSockets<TheSchema>("/graphql");
+            app.UseGraphQLWebSockets<TheSchema> ("/graphql");
 
             // Enable endpoint for querying
-            app.UseGraphQL<TheSchema>("/graphql");
+            app.UseGraphQL<TheSchema> ("/graphql");
 
             // Enable developer UI
-            app.UseGraphiQLServer(new GraphiQLOptions { GraphiQLPath = "/ui" });
+            app.UseGraphiQLServer (new GraphiQLOptions { GraphiQLPath = "/ui" });
 
-            app.UseRouting();
+            app.UseRouting ();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
             });
         }
     }
